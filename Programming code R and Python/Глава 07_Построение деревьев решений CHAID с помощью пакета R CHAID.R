f5a13238dac6ab225341277f02b96f06e36ce99e
Gewissta <hadoop.hadoop@yandex.ru>
2017-09-29 19:32:34 +0300

    update

--- Programming code R and Python/Глава 07_Построение деревьев решений CHAID с помощью пакета R CHAID.R
+++ Programming code R and Python/Глава 07_Построение деревьев решений CHAID с помощью пакета R CHAID.R
@@ -1,17 +1,17 @@
-# Глава 7 Построение деревьев решений CHAID с помощью пакета R CHAID
+# Р“Р»Р°РІР° 7 РџРѕСЃС‚СЂРѕРµРЅРёРµ РґРµСЂРµРІСЊРµРІ СЂРµС€РµРЅРёР№ CHAID СЃ РїРѕРјРѕС‰СЊСЋ РїР°РєРµС‚Р° R CHAID
 
-# 7.1 Построение и интерпретация дерева классификации CHAID
+# 7.1 РџРѕСЃС‚СЂРѕРµРЅРёРµ Рё РёРЅС‚РµСЂРїСЂРµС‚Р°С†РёСЏ РґРµСЂРµРІР° РєР»Р°СЃСЃРёС„РёРєР°С†РёРё CHAID
 
-# 7.1.1 Подготовка данных
+# 7.1.1 РџРѕРґРіРѕС‚РѕРІРєР° РґР°РЅРЅС‹С…
 
-# Задаем постоянный CRAN репозиторий
+# Р—Р°РґР°РµРј РїРѕСЃС‚РѕСЏРЅРЅС‹Р№ CRAN СЂРµРїРѕР·РёС‚РѕСЂРёР№
 cat(".Rprofile: Setting US repositoryn")
 r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
 rm(r)
 
-# Устанавливаем необходимые для работы пакеты
+# РЈСЃС‚Р°РЅР°РІР»РёРІР°РµРј РЅРµРѕР±С…РѕРґРёРјС‹Рµ РґР»СЏ СЂР°Р±РѕС‚С‹ РїР°РєРµС‚С‹
 install.packages("CHAID", repos="http://R-Forge.R-project.org")
 install.packages("pROC")
 install.packages("rattle")
@@ -34,7 +34,7 @@ data$agecat <- NULL
 
 str(data)
 
-# 7.1.2 Построение модели и работа с диаграммой дерева
+# 7.1.2 РџРѕСЃС‚СЂРѕРµРЅРёРµ РјРѕРґРµР»Рё Рё СЂР°Р±РѕС‚Р° СЃ РґРёР°РіСЂР°РјРјРѕР№ РґРµСЂРµРІР°
 
 set.seed(42)
 library(CHAID)
@@ -42,13 +42,13 @@ library(CHAID)
 model.chaid  <- chaid(churn ~ ., 
                       control = chaid_control
                       (minprob = 0.001,
-                      minsplit = 500,minbucket = 250),
+                        minsplit = 500,minbucket = 250),
                       data)
 
 plot(model.chaid)
 print(model.chaid)
 
-# 7.1.3 Вычисление вероятностей классов и выбор оптимального порога
+# 7.1.3 Р’С‹С‡РёСЃР»РµРЅРёРµ РІРµСЂРѕСЏС‚РЅРѕСЃС‚РµР№ РєР»Р°СЃСЃРѕРІ Рё РІС‹Р±РѕСЂ РѕРїС‚РёРјР°Р»СЊРЅРѕРіРѕ РїРѕСЂРѕРіР°
 
 predict(model.chaid, data, type="prob")
 predict(model.chaid, data, type="prob")[4427:4431]
@@ -60,41 +60,41 @@ plot(roc)
 roc
 
 roc2 <- plot.roc(data$churn, score[,1],
-        main="ROC-кривая для модели дерева CHAID",  
-        percent=TRUE, ci=TRUE, # вычислить AUC
-        print.auc=TRUE) # напечатать значение AUC (вместе с довер. интервалом)
+                 main="ROC-РєСЂРёРІР°СЏ РґР»СЏ РјРѕРґРµР»Рё РґРµСЂРµРІР° CHAID",  
+                 percent=TRUE, ci=TRUE, # РІС‹С‡РёСЃР»РёС‚СЊ AUC
+                 print.auc=TRUE) # РЅР°РїРµС‡Р°С‚Р°С‚СЊ Р·РЅР°С‡РµРЅРёРµ AUC (РІРјРµСЃС‚Рµ СЃ РґРѕРІРµСЂ. РёРЅС‚РµСЂРІР°Р»РѕРј)
 
 ci.thresholds(roc)
-coords(roc, # ROC-кривая модели 
-       "best", # вид критерия 
-       best.method="youden", # способ расчета критерия
-       ret=c("threshold", "spec", "sens")) # какие параметры возвращать
+coords(roc, # ROC-РєСЂРёРІР°СЏ РјРѕРґРµР»Рё 
+       "best", # РІРёРґ РєСЂРёС‚РµСЂРёСЏ 
+       best.method="youden", # СЃРїРѕСЃРѕР± СЂР°СЃС‡РµС‚Р° РєСЂРёС‚РµСЂРёСЏ
+       ret=c("threshold", "spec", "sens")) # РєР°РєРёРµ РїР°СЂР°РјРµС‚СЂС‹ РІРѕР·РІСЂР°С‰Р°С‚СЊ
 
 data.t <- table(data$churn)
 w <- as.numeric(data.t[2]/(data.t[1]+data.t[2]))
 
-coords(roc, # ROC-кривая модели
-       "best", # вид критерия
-       best.method="youden", # способ расчета критерия
-       best.weights=c(1, w), # подставляем вычисленную долю ушедших в выборке
-       ret=c("threshold", "spec", "sens")) # какие параметры возвращать
+coords(roc, # ROC-РєСЂРёРІР°СЏ РјРѕРґРµР»Рё
+       "best", # РІРёРґ РєСЂРёС‚РµСЂРёСЏ
+       best.method="youden", # СЃРїРѕСЃРѕР± СЂР°СЃС‡РµС‚Р° РєСЂРёС‚РµСЂРёСЏ
+       best.weights=c(1, w), # РїРѕРґСЃС‚Р°РІР»СЏРµРј РІС‹С‡РёСЃР»РµРЅРЅСѓСЋ РґРѕР»СЋ СѓС€РµРґС€РёС… РІ РІС‹Р±РѕСЂРєРµ
+       ret=c("threshold", "spec", "sens")) # РєР°РєРёРµ РїР°СЂР°РјРµС‚СЂС‹ РІРѕР·РІСЂР°С‰Р°С‚СЊ
 
 coords(roc,"best", best.method="youden",
-       best.weights=c(2, 0.5), # первый параметр увеличен вдвое
+       best.weights=c(2, 0.5), # РїРµСЂРІС‹Р№ РїР°СЂР°РјРµС‚СЂ СѓРІРµР»РёС‡РµРЅ РІРґРІРѕРµ
        ret=c("threshold", "spec", "sens"))
 
-# 7.1.4 Получение спрогнозированных классов зависимой переменной
+# 7.1.4 РџРѕР»СѓС‡РµРЅРёРµ СЃРїСЂРѕРіРЅРѕР·РёСЂРѕРІР°РЅРЅС‹С… РєР»Р°СЃСЃРѕРІ Р·Р°РІРёСЃРёРјРѕР№ РїРµСЂРµРјРµРЅРЅРѕР№
 
 predict(model.chaid, data, type="response")[4427:4431]
 result <- predict(model.chaid, data, type="response")
 table(data$churn, result)
 
-# 7.1.5 Сохранение прогнозов
+# 7.1.5 РЎРѕС…СЂР°РЅРµРЅРёРµ РїСЂРѕРіРЅРѕР·РѕРІ
 
 churnprob<-data.frame(data, result=score)
 write.csv(churnprob, "churnprob.csv")
 
-# 7.1.6 Применение модели к новым данным
+# 7.1.6 РџСЂРёРјРµРЅРµРЅРёРµ РјРѕРґРµР»Рё Рє РЅРѕРІС‹Рј РґР°РЅРЅС‹Рј
 
 churnprognoz <- read.csv2("C:/Trees/Churn_new.csv")
 str(churnprognoz)
@@ -117,9 +117,9 @@ newclients_churnprognoz<-data.frame(churnprognoz, result=newscore)
 write.csv(newclients_churnprognoz,
           "newclients_churnprognoz.csv")
 
-# 7.1.7 Проверка модели
+# 7.1.7 РџСЂРѕРІРµСЂРєР° РјРѕРґРµР»Рё
 
-# 7.1.7.1 Однократное случайное разбиение набора данных на обучающую и контрольную выборки
+# 7.1.7.1 РћРґРЅРѕРєСЂР°С‚РЅРѕРµ СЃР»СѓС‡Р°Р№РЅРѕРµ СЂР°Р·Р±РёРµРЅРёРµ РЅР°Р±РѕСЂР° РґР°РЅРЅС‹С… РЅР° РѕР±СѓС‡Р°СЋС‰СѓСЋ Рё РєРѕРЅС‚СЂРѕР»СЊРЅСѓСЋ РІС‹Р±РѕСЂРєРё
 
 data <- read.csv2("C:/Trees/Churn_binned.csv")
 
@@ -146,7 +146,7 @@ library(CHAID)
 chaid.model  <- chaid(churn ~ . , 
                       control = chaid_control
                       (minprob = 0.001,
-                      minsplit = 500, minbucket = 250),
+                        minsplit = 500, minbucket = 250),
                       data=development)
 
 score_dev <- predict(chaid.model, development, type = "prob")
@@ -156,9 +156,9 @@ library(pROC)
 
 roc_dev <-plot(roc(development$churn, score_dev[,1], ci=TRUE), percent=TRUE, print.auc=TRUE, col="#1c61b6")
 roc_hold <-plot(roc(holdout$churn, score_hold[,1], ci=TRUE), percent=TRUE, 
-         print.auc=TRUE, col="#008600", print.auc.y= .4, add=TRUE)
-# Создаем легенды к ROC-кривым.
-legend("bottomright", legend=c("Обучающая выборка", "Контрольная выборка"), 
+                print.auc=TRUE, col="#008600", print.auc.y= .4, add=TRUE)
+# РЎРѕР·РґР°РµРј Р»РµРіРµРЅРґС‹ Рє ROC-РєСЂРёРІС‹Рј.
+legend("bottomright", legend=c("РћР±СѓС‡Р°СЋС‰Р°СЏ РІС‹Р±РѕСЂРєР°", "РљРѕРЅС‚СЂРѕР»СЊРЅР°СЏ РІС‹Р±РѕСЂРєР°"), 
        col=c("#1c61b6", "#008600"), lwd=2)
 
 result_dev <- predict(chaid.model, development, type="response")
@@ -167,7 +167,7 @@ table(development$churn, result_dev)
 result_hold <- predict(chaid.model, holdout, type="response")
 table(holdout$churn, result_hold)
 
-# 7.1.7.2 Многократное случайное разбиение набора данных на обучающую и контрольную выборки
+# 7.1.7.2 РњРЅРѕРіРѕРєСЂР°С‚РЅРѕРµ СЃР»СѓС‡Р°Р№РЅРѕРµ СЂР°Р·Р±РёРµРЅРёРµ РЅР°Р±РѕСЂР° РґР°РЅРЅС‹С… РЅР° РѕР±СѓС‡Р°СЋС‰СѓСЋ Рё РєРѕРЅС‚СЂРѕР»СЊРЅСѓСЋ РІС‹Р±РѕСЂРєРё
 
 data <- read.csv2("C:/Trees/Churn_binned.csv")
 data$longdist2 <- ordered(data$longdist, levels = c("<2", "2--8", " 9--14", "15-20", "21+"))
@@ -191,7 +191,7 @@ for (i in 1:1000) {
   chaid.model  <- chaid(churn ~ . , 
                         control = chaid_control
                         (minprob = 0.001,
-                        minsplit = 500, minbucket = 250),
+                          minsplit = 500, minbucket = 250),
                         data=development)
   chaid.score <- predict(chaid.model, holdout, type = "prob")
   chaid.roc <- roc(holdout$churn, chaid.score[,1])
@@ -201,7 +201,7 @@ for (i in 1:1000) {
 mean(chaid.auc)
 ci.auc(chaid.roc)
 
-# 7.1.7.3 Перекрестная проверка
+# 7.1.7.3 РџРµСЂРµРєСЂРµСЃС‚РЅР°СЏ РїСЂРѕРІРµСЂРєР°
 
 data <- read.csv2("C:/Trees/Churn_binned.csv")
 data$longdist2 <- ordered(data$longdist, levels = c("<2", "2--8", " 9--14", "15-20", "21+"))
@@ -220,31 +220,31 @@ ind = cut(1:nrow(data), breaks=10, labels=F)
 
 accuracies = c()
 for (i in 1:10)  {
-fit = chaid(churn ~., control = chaid_control (minprob = 0.001, 
-            minsplit = 500, minbucket = 250), data[ind != i,])
-predictions = predict(fit, data[ind == i, ! names(data) %in% c("churn")])
-correct_count = sum(predictions == data[ind == i,c("churn")])
-accuracies = append(correct_count / nrow(data[ind == i,]), accuracies)
+  fit = chaid(churn ~., control = chaid_control (minprob = 0.001, 
+                                                 minsplit = 500, minbucket = 250), data[ind != i,])
+  predictions = predict(fit, data[ind == i, ! names(data) %in% c("churn")])
+  correct_count = sum(predictions == data[ind == i,c("churn")])
+  accuracies = append(correct_count / nrow(data[ind == i,]), accuracies)
 }
 
 accuracies
 
 mean(accuracies)
 
-# 7.2 Биннинг переменных
+# 7.2 Р‘РёРЅРЅРёРЅРі РїРµСЂРµРјРµРЅРЅС‹С…
 
-# 7.2.1 Биннинг в пакете rattle
+# 7.2.1 Р‘РёРЅРЅРёРЅРі РІ РїР°РєРµС‚Рµ rattle
 
 data <- read.csv2("C:/Trees/Churn_rattle.csv")
 library(rattle)
 str(data)
 
 longdist2<- binning(data$longdist, bins=3,
-            method="quantile", labels=NULL, 
-            ordered=TRUE, weights=NULL)
+                    method="quantile", labels=NULL, 
+                    ordered=TRUE, weights=NULL)
 age2<- binning(data$age, bins=5, 
-       method="quantile", labels=NULL, 
-       ordered=TRUE, weights=NULL)
+               method="quantile", labels=NULL, 
+               ordered=TRUE, weights=NULL)
 
 data<-data.frame(data, longdist2)
 data<-data.frame(data, age2)
@@ -254,7 +254,7 @@ data$age<- NULL
 
 str(data)
 
-# 7.2.2 Биннинг в пакете smbinning
+# 7.2.2 Р‘РёРЅРЅРёРЅРі РІ РїР°РєРµС‚Рµ smbinning
 
 data <- read.csv2("C:/Trees/Churn_smbinning.csv")
 library(smbinning)
@@ -264,7 +264,7 @@ result=smbinning (df=data, y="churn", x="age")
 
 smbinning.sql(result)
 smbinning.plot(ivout=result, option="WoE", 
-               sub="Переменная age")
+               sub="РџРµСЂРµРјРµРЅРЅР°СЏ age")
 result2=smbinning.sumiv(df=data, y="churn")
 result2
 smbinning.sumiv.plot(sumivt=result2, cex=0.9)
@@ -272,6 +272,3 @@ smbinning.sumiv.plot(sumivt=result2, cex=0.9)
 data=smbinning.gen(df=data,ivout=result,
                    chrname="agebin")
 data$age<- NULL
-
-
-
